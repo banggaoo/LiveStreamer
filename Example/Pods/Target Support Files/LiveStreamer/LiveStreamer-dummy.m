778d9422a0280c1a12865b832c18af9f364aa4af
dongseoklee <banggaoo@naver.com>
2018-07-20 10:24:25 +0900

    Prepare to create CocoaPod repository

---
+++ Example/Pods/Target Support Files/LiveStreamer/LiveStreamer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LiveStreamer : NSObject
+@end
+@implementation PodsDummy_LiveStreamer
+@end
---
+++ Example/Pods/Target Support Files/Pods-LiveStreamer_Tests/Pods-LiveStreamer_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LiveStreamer_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LiveStreamer_Tests
+@end
